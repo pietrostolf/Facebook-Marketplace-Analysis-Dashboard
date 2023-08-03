7565d9bf38693e405db79f717fead2e9795532a6
pietrostolf <124335112+pietrostolf@users.noreply.github.com>
2023-08-03 17:50:27 -0400

    Finished round 2 of testing and validating (checkbox added)

---
+++ Application/app.py
@@ -0,0 +1,66 @@
+from flask import Flask, jsonify
+from sqlalchemy import create_engine, Column, Integer, String, Boolean
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from flask_cors import CORS
+
+# Define the VehicleListing class
+Base = declarative_base()
+
+class VehicleListing(Base):
+    __tablename__ = 'vehicle_listings'
+
+    id = Column(Integer, primary_key=True)
+    City = Column(String)
+    Province = Column(String)
+    Price = Column(Integer)
+    Year = Column(Integer)
+    Make = Column(String)
+    Model = Column(String)
+    Mileage = Column(Integer)
+    URL = Column(String)
+    Rebuilt_Predicted = Column(Boolean)
+
+# Create the engine and session
+engine = create_engine('postgresql://postgres:140494@localhost:5433/vehicle_clean_labeled_2')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Create the Flask app
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api', methods=['GET'])
+def get_all_vehicles():
+    # Query all vehicle listings from the database
+    all_entries = session.query(VehicleListing).all()
+
+    # Convert the vehicle listings to a list of dictionaries
+    vehicles = []
+    for entry in all_entries:
+        vehicle = {
+            'id': entry.id,
+            'City': entry.City,
+            'Province': entry.Province,
+            'Price': entry.Price,
+            'Year': entry.Year,
+            'Make': entry.Make,
+            'Model': entry.Model,
+            'Mileage': entry.Mileage,
+            'URL': entry.URL,
+            'Rebuilt_Predicted': entry.Rebuilt_Predicted
+        }
+        vehicles.append(vehicle)
+
+    # Create the response data
+    response_data = {
+        'listings': vehicles
+    }
+
+    if vehicles:
+        return jsonify(response_data)
+    else:
+        return jsonify({'message': 'No entries found in the database.'})
+
+if __name__ == '__main__':
+    app.run()
